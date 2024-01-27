fce4499ca4c25d0d80025d23ea4710ab05e2e816
eduardoccarmo <eduardo.ccarmo@outlook.com>
2024-01-27 13:23:10 -0300

    Implementando o repositório e repositórios genéricos.

---
+++ src/DevIo.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,31 @@
+﻿using DevIo.Business.Models;
+using DevIo.Data.Context;
+using DevIO.Business.Interfaces;
+
+namespace DevIo.Data.Repository
+{
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
+    {
+        public FornecedorRepository(MeuDbContext db) : base(db) { }
+
+        public Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task RemoverEnderecoFornecedor(Endereco endereco)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DevIo.Data/Repository/ProdutoRepository.cs
@@ -0,0 +1,34 @@
+﻿using DevIo.Business.Models;
+using DevIo.Data.Context;
+using DevIO.Business.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Xml.Linq;
+
+namespace DevIo.Data.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(MeuDbContext db) : base(db) { }
+
+        public async Task<Produto> ObterProdutoFornecedor(Guid id)
+        {
+            return await Db.Produtos.AsNoTracking()
+                 .Include(f => f.Fornecedor)
+                 .FirstOrDefaultAsync(p => p.Id == id);
+
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosFornecedores()
+        {
+            return await Db.Produtos.AsNoTracking()
+                 .Include(f => f.Fornecedor)
+                 .OrderBy(p => p.Nome)
+                 .ToListAsync(); 
+        }
+
+        public async Task<IEnumerable<Produto>> ObterProdutosPorFornecedor(Guid fornecedorId)
+        {
+            return await Buscar(p => p.FornecedorId == fornecedorId);
+        }
+    }
+}
