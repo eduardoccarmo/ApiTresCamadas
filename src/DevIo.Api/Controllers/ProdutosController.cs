aab08a7ddc0dde14e943bf56872e5f27d7b4d610
eduardoccarmo <eduardo.ccarmo@outlook.com>
2024-01-30 13:58:37 -0300

    Implementando as controllers.

---
+++ src/DevIo.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,44 @@
+﻿using DevIo.Api.Controllers;
+using DevIo.Api.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/produtos")]
+    public class ProdutosController : MainController
+    {
+        public ProdutosController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid id)
+        {
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoViewModel>> Adicionar(ProdutoViewModel produtoViewModel)
+        {
+
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Atualizar(Guid id, ProdutoViewModel produtoViewModel)
+        {
+
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> Excluir(Guid id)
+        {
+
+        }
+    }
+}
